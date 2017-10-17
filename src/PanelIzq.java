0dd2a109483169b78fe89db48790d62722e0b06e
Unknown <luisl6067@gmail.com>
2017-10-17 08:23:27 -0500

    clases

--- src/PanelIzq.java
+++ src/PanelIzq.java
@@ -1,4 +1,131 @@
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
 
-public class PanelIzq {
-
+import javax.swing.*;
+import javax.swing.border.Border;
+public class PanelIzq extends JPanel {
+private VentanaInicial referenciaVentaPrincipal;
+	private GridBagConstraints c;
+	private Insets in;
+	private JLabel JLnombre,JLapellido,JLtipo,JLnumero,JLedad,JLpeso,JLaltura,JLcelular,JLdirrecion,JLpuesto;
+	private JTextField JTnombre,JTapellido,JTtipo,JTnumero,JTedad,JTpeso,JTaltura,JTcelular,JTdirrecion,JTpuesto;
+	JButton Bagregar;
+	public PanelIzq(VentanaInicial _referenciaVentaPrincipal) {
+		referenciaVentaPrincipal = _referenciaVentaPrincipal;
+	
+		setLayout(new GridBagLayout());
+		Border borde1 = BorderFactory.createTitledBorder("Formulario");
+		setBorder(borde1);
+		
+		JLnombre= new JLabel("Nombre:");
+		c = new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=0;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets(5,10,5,10);
+		c.insets= in;
+		add(JLnombre,c);
+		
+		JTnombre = new JTextField();
+		c= new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=1;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets (5,10,5,10);
+		c.insets = in ;
+		add(JTnombre,c);
+		
+		JLapellido= new JLabel("Apellido:");
+		c = new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=2;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets(5,10,5,10);
+		c.insets= in;
+		add(JLapellido,c);
+		
+		JTapellido = new JTextField();
+		c= new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=3;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets (5,10,5,10);
+		c.insets = in ;
+		add(JTapellido,c);
+		
+		JLtipo= new JLabel("Nombre:");
+		c = new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=4;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets(5,10,5,10);
+		c.insets= in;
+		add(JLtipo,c);
+		
+		JTtipo = new JTextField();
+		c= new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=5;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets (5,10,5,10);
+		c.insets = in ;
+		add(JTtipo,c);
+		
+		JLnumero= new JLabel("Nombre:");
+		c = new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=6;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets(5,10,5,10);
+		c.insets= in;
+		add(JLnumero,c);
+		
+		JTnumero = new JTextField();
+		c= new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=7;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets (5,10,5,10);
+		c.insets = in ;
+		add(JTnumero,c);
+		
+		JLedad= new JLabel("Nombre:");
+		c = new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=8;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets(5,10,5,10);
+		c.insets= in;
+		add(JLedad,c);
+		
+		JTedad = new JTextField();
+		c= new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=9;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets (5,10,5,10);
+		c.insets = in ;
+		add(JTedad,c);
+		
+		JLpeso= new JLabel("Nombre:");
+		c = new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=10;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets(5,10,5,10);
+		c.insets= in;
+		add(JLpeso,c);
+		
+		JTpeso = new JTextField();
+		c= new GridBagConstraints();
+		c.gridx=0;
+		c.gridy=1;
+		c.fill = GridBagConstraints.BOTH;
+		in = new Insets (5,10,5,10);
+		c.insets = in ;
+		add(JTpeso,c);
+		
+		
+	}
 }
--- src/VentanaInicial.java
+++ src/VentanaInicial.java
@@ -1,4 +1,38 @@
+import javax.swing.JFrame;
+import java.awt.*;
+import java.util.ArrayList;
+public class VentanaInicial extends JFrame {
+	public ArrayList<Persona> listapersona;
+	
+	
+	
+	public VentanaInicial() {
+		super("medallas");
+		listapersona= new ArrayList<Persona>();
+		inicio();
+		componentes();
+		pack();
+		setLocationRelativeTo(null);
+		
+	}
 
-public class VentanaInicial {
 
+
+	private VentanaInicial inicio() {
+		Dimension dims = new Dimension(1000,700);
+		this.setSize(dims);
+		this.setPreferredSize(dims);
+		this.getContentPane().setLayout(new BorderLayout());
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		return this;
+	}
+
+
+
+	private void componentes() {
+		PanelIzq obj_panelIzq = new PanelIzq(this);
+		add(obj_panelIzq,BorderLayout.WEST);
+	}
+	
+	
 }
