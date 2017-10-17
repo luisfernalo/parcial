aa11ee2b6746688d23bafac756bf77b65028755d
Unknown <luisl6067@gmail.com>
2017-10-17 08:22:28 -0500

    a

--- src/Principal.java
+++ src/Principal.java
@@ -2,7 +2,8 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		VentanaInicial obj_ventana= new VentanaInicial();
+		obj_ventana.setVisible(true);
 
 	}
 
